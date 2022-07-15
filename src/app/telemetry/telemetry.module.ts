983633bfd36d232a4933a649e1d6cfcd5b9128db
alvalea <alvalea@gmail.com>
2022-07-15 16:18:45 +0200

    Remove already loaded module in telemetry

--- src/app/telemetry/telemetry.module.ts
+++ src/app/telemetry/telemetry.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { TelemetryRoutingModule } from './telemetry-routing.module';
 
@@ -8,8 +6,6 @@ import { TelemetryComponent } from './telemetry.component';
 
 @NgModule({
   imports: [
-    BrowserModule,
-    HttpClientModule,
     TelemetryRoutingModule
   ],
   declarations: [
